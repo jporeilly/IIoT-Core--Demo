38c93a17b789be7626cc60b6b142a6ca5121b584
James O'Reilly <jp.oreilly@hotmail.com>
2022-05-09 20:04:36 +0100

    Updates

--- 01--Pre-flight/pre-flight_iiot-core.sh
+++ 01--Pre-flight/pre-flight_iiot-core.sh
@@ -4,7 +4,7 @@
 # Edit your /etc/hosts file to resolve IP and FQDN. 
 # Pre-requisite steps: disable swap
 #                      disable firewall (demo only)
-# Install packages:
+# Install packages:    Yq
 #                      Jq
 #                      Helm
 #
@@ -23,8 +23,9 @@
 # ==============================================================
 
 # Infrastructure
-apt update -y
-apt upgrade -y
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+dnf update -y
+dnf upgrade -y
 swapoff --all
 sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 ufw disable # Do not disable in Production.
@@ -34,21 +35,27 @@ sleep 3s
 echo -e "The server requires a reboot .."
 reboot
 
+# Install Yq
+snap install yq
+
 # Install Jq
-apt-get install -y jq
+snap install -y jq
 
 # Install Helm
 snap install helm --classic
 
 # Docker pre-requisites
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+dnf install -y yum-utils
+dnf config-manager –add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf update
+dnf repolist
+sleep 10s
+dnf remove podman buildah
 echo -e "Pre-requisite installations completed .."
 
 # Install Latest Stable Docker Release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update -y
-apt-get install -y docker-ce docker-ce-cli containerd.io
+dnf install docker-ce docker-ce-cli containerd.io
+yum install docker-ce --allowerasing -y
 tee /etc/docker/daemon.json >/dev/null <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -60,28 +67,11 @@ tee /etc/docker/daemon.json >/dev/null <<EOF
   "storage-driver": "overlay2"
 }
 EOF
-mkdir -p /etc/systemd/system/docker.service.d
+systemctl start docker.service
+systemctl enable docker.service
 groupadd docker
 MAINUSER=$(logname)
 usermod -aG docker $MAINUSER
 systemctl daemon-reload
 systemctl restart docker
 echo -e "Docker Installation completed .."
-
-# Install Latest Stable Docker Compose Release
-COMPOSEVERSION=$(curl -s https://github.com/docker/compose/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
-curl -L "https://github.com/docker/compose/releases/download/v$COMPOSEVERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-echo "Docker Compose Installation completed .."
-
-# Install Docker Registry
-mkdir -p /data/docker-registry
-cp /Downloads/docker-compose.yaml /data/docker-registry
-cd /data/docker-registry
-docker-compose up -d
-echo "Docker Registry Installation completed .."
-
-sleep 3s
-echo "The server requires a reboot .."
-reboot
