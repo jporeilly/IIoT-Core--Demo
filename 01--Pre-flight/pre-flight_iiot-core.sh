ca615250164962da82c22adc4d4d6314e289f498
James O'Reilly <jp.oreilly@hotmail.com>
2022-05-18 17:18:56 +0100

    Updates

--- 01--Pre-flight/pre-flight_iiot-core.sh
+++ 01--Pre-flight/pre-flight_iiot-core.sh
@@ -4,8 +4,12 @@
 # Edit your /etc/hosts file to resolve IP and FQDN. 
 # Pre-requisite steps: disable swap
 #                      disable firewall (demo only)
+<<<<<<< Updated upstream:01--Pre-flight/pre-flight_iiot-core.sh
 # Install packages:    Yq
 #                      Jq
+=======
+# Install packages:
+>>>>>>> Stashed changes:01--Pre-flight/pre-flight_dc.sh
 #                      Helm
 #
 # Pre-requistes steps for Docker: 
@@ -19,16 +23,19 @@
 # Install Docker Compose
 # Install Docker Registry 
 #
-# 24/04/2022
+# /05/2022
 # ==============================================================
 
 # Infrastructure
+<<<<<<< Updated upstream:01--Pre-flight/pre-flight_iiot-core.sh
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+=======
+>>>>>>> Stashed changes:01--Pre-flight/pre-flight_dc.sh
 dnf update -y
 dnf upgrade -y
 swapoff --all
 sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
-ufw disable # Do not disable in Production.
+systemctl disable firewalld # Do not disable in Production.
 echo -e "Infrastructure update completed .."
 
 sleep 3s
@@ -39,11 +46,16 @@ reboot
 snap install yq
 
 # Install Jq
+<<<<<<< Updated upstream:01--Pre-flight/pre-flight_iiot-core.sh
 snap install -y jq
+=======
+dnf install -y jq
+>>>>>>> Stashed changes:01--Pre-flight/pre-flight_dc.sh
 
 # Install Helm
 snap install helm --classic
 
+<<<<<<< Updated upstream:01--Pre-flight/pre-flight_iiot-core.sh
 # Docker pre-requisites
 dnf install -y yum-utils
 dnf config-manager –add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -56,14 +68,23 @@ echo -e "Pre-requisite installations completed .."
 # Install Latest Stable Docker Release
 dnf install docker-ce docker-ce-cli containerd.io
 yum install docker-ce --allowerasing -y
+=======
+# Install Docker
+dnf install 
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf update -y
+dnf install --nobest -y docker-ce docker-ce-cli containerd.io
+>>>>>>> Stashed changes:01--Pre-flight/pre-flight_dc.sh
 tee /etc/docker/daemon.json >/dev/null <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
-  "insecure-registries" : ["localhost:5000","0.0.0.0/0"],
+  "insecure-registries" : ["iiot-core:5000","0.0.0.0/0"],
   "log-driver": "json-file",
   "log-opts": {
     "max-size": "100m"
   },
+
+
   "storage-driver": "overlay2"
 }
 EOF
