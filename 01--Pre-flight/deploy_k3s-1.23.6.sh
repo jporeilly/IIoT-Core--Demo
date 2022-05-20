dbe958d0bcbff17f70b21b4660bd7ce1a554c940
James O'Reilly <jp.oreilly@hotmail.com>
2022-05-20 11:19:15 +0100

    Updates

--- 01--Pre-flight/deploy_k3s-1.23.6.sh
+++ 01--Pre-flight/deploy_k3s-1.23.6.sh
@@ -2,7 +2,7 @@
 
 # ==============================================================
 #
-# Install k3s -disable traefik
+# Deploy k3s -disable traefik
 # Configure kubectl 
 #
 # 20/05/2022
@@ -12,13 +12,14 @@
 curl https://get.k3s.io | sudo INSTALL_K3S_COMMIT=$COMMIT INSTALL_K3S_TYPE=server sh -s - --cluster-init --disable traefik
 
 # Connect and test kubectl
-chown -R pentaho /etc/rancher/k3s/k3s.yaml
+chown -R k8s /etc/rancher/k3s/k3s.yaml
+mkdir ~/.kube
 cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-chown $USER:$GROUP ~/.kube/config
+chown -R $USER:$GROUP ~/.kube/config
 systemctl enable k3s
 kubectl get pods -A
 echo -e "k3s Installation Completed .."
 
-sleep 3s
+sleep 2s
 echo -e "Reboot required .."
 reboot
